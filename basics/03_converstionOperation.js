b3479830e88d3ff9f7a906c188dbdf736e91997f
Varun <120800038+varun014@users.noreply.github.com>
2026-04-26 14:12:52 +0000

    conversition operations

--- basics/03_converstionOperation.js
+++ basics/03_converstionOperation.js
@@ -13,8 +13,50 @@ console.log(valueInNumber)
 
 let isLoggedIn = "hi"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 // 1 => true; 0 => false
 // "" => false
 // "varun" => true
+
+// ********************** Operations *************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3);
+
+
+
+let str1 = "hello"
+let str2 = "varun"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log(+true);
+console.log(+"");
+
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2 // smart but no for professtional use
+
+let gameCounter = 100;
+gameCounter++;
+
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
